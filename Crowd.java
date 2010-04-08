62785a26a8526a66c94b3e8eff8588b9f35be08a
mrprlt99 <mrprlt99@15e78a1c-1ffc-11df-970b-617f64f7e5c1>
2010-04-08 12:12:58 +0000

    added person and crowd classes with getter/setter functions

--- Crowd.java
+++ Crowd.java
@@ -1,4 +1,39 @@
-// this class file implements the Crowd type
-public class Crowd {
 
+import java.util.ArrayList;
+
+public class Crowd {
+	int Size;
+	int EnergyLevel;
+	int ThrillLevel;
+	int SpendingCapacity;
+	ArrayList<Person> people;
+	
+	private void setattributes(int size,int energy,int thrill,int spend){
+		Size = size;
+		EnergyLevel = energy;
+		ThrillLevel = thrill;
+		SpendingCapacity = spend;
+	}
+	
+	public Crowd(){
+		people=new ArrayList<Person>();
+		setattributes(10,10,5,50);//default values?
+		for(int i = 0; i<Size;i++){
+			//TODO:set attributes of each created person in a normal distribution of crowd attributes
+			people.add(new Person());
+		}
+	}
+
+	public int getEnergyLevel(){
+		return EnergyLevel;
+	}
+	
+	public int getThrillLevel(){
+		return ThrillLevel;
+	}
+	
+	public int getSpendingCapacity(){
+		return SpendingCapacity;
+	}
+	
 }
