46737aecb03c7867f26e36d0ab80406d5d930c46
mrprlt99 <mrprlt99@15e78a1c-1ffc-11df-970b-617f64f7e5c1>
2010-04-14 05:44:11 +0000

    added gui files

---
+++ trunk/java_backend/ui.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ui {
+	public static void main (String[] args){
+	
+		TextAreaDemo text = new TextAreaDemo();
+		text.setVisible(true); 
+		
+	}
+}
+
