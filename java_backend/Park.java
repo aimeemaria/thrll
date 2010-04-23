719296d8586f9aa3cd6dd06d66cb22bc819fbda3
USFFanatic <USFFanatic@15e78a1c-1ffc-11df-970b-617f64f7e5c1>
2010-04-23 18:27:07 +0000

    Updated simulation.  Works better, but I suspect there's a bug.  I'll keep working on it, but I wanted to upload to the repository.

--- java_backend/Park.java
+++ java_backend/Park.java
@@ -15,10 +15,10 @@ public class Park
 	private double cost;
 	public int hours;  		//the number of hours the park operates in a day.
 	public double salary;  	//the salary of park employees
-	
+
 	// internal list of land objects
 	private ArrayList<Land> LandObjs = new ArrayList<Land>();
-	
+
 	/*	Constructor
 	 *	to set default values of the members
 	 */
@@ -75,52 +75,54 @@ public class Park
 	public double calculateRevenue(Crowd c, Duration d)
 	{
 		double result = 0;
-		
-		// write the revenue logic here. This function should be able to 
-		// access other objects as well. we need to access tha land objects
-		// via landObjs arraylist. Each class should have a copy of its children
-		// objects
-		
+
 		//for each land
 		//get cost of operation (include employees)
 		double cost=0;
 		for(int i = 0 ; i < LandObjs.size();i++){
 			cost += LandObjs.get(i).getcost(hours,salary);//returns operation cost for all elements in the land
 		}
+
 		//assume the crowd enters as soon as park opens
-		
 		c.createpeople();
-		//ToDo:
-		//run simulation of decisions for each person and update revenue
 		Person p;
 		Random generator = new Random();  //Random Number Generator
 		for(int i =0;i<c.getSize();i++){
 			p = c.people.get(i);
+			//*********************************************************
+			//TESTING CODE.  REMOVE ALL CODE BETWEEN HERE AFTER TESTING
+			//*********************************************************
+			System.out.println("Energy = " +p.getEnergyLevel());
+			System.out.println("Thrill = " +p.getThrillLevel());
+			System.out.println("Spending = " +p.getSpendingCapacity());
+			//*********************************************************
 			int num_Lands = LandObjs.size();
 			int decide;
 			//Person p will be simulated for the entire Duration
 			while (p.getTick()>0){
+
+				//*********************************************************
+				//TESTING CODE.  REMOVE ALL CODE BETWEEN HERE AFTER TESTING
+				//*********************************************************
 				System.out.println("Tick = " +p.getTick());
+				//*********************************************************
+
 				//Choose land to visit
 				int land_choice = generator.nextInt(num_Lands);
 				//Choose Attraction, Store, or Restaurant
 				int type_choice = generator.nextInt(3);
-				//An ArrayList of lands that do not contain elements of this choice
-				ArrayList<Integer> emptyLands = new ArrayList<Integer>();
-				
+
 				if (type_choice == 0) {  //choose attraction
-					ArrayList<LandElement> attractions = getAllType(LandObjs.get(land_choice), 'a');
-					if (attractions.size()==0){
-						emptyLands.add(land_choice);
-						if (emptyLands.size() != LandObjs.size()){
-							do{
-								land_choice = generator.nextInt(num_Lands);	
-							}
-							while(!emptyLands.contains(land_choice));
-						}
-						else
-							p.leavePark();
-						}
+					
+					//*********************************************************
+					//TESTING CODE.  REMOVE ALL CODE BETWEEN HERE AFTER TESTING
+					//*********************************************************
+					System.out.println("Wants to ride a ride.");
+					//*********************************************************
+					ArrayList<LandElement> attractions = getPossibleChoices(land_choice, 'a');
+					if(attractions.size() == 0){
+						p.leavePark();
+					}
 					else{
 						int choice = generator.nextInt(attractions.size());
 						LandElement attraction = attractions.get(i);
@@ -129,59 +131,58 @@ public class Park
 							p.decreaseTick(1);
 						else
 							p.decreaseTick(2);
-						
+
 						if(attraction.canEnter(p)){
 							attraction.enter(p);
 							System.out.println("Entered Attraction");
 							p.decreaseTick(attraction.getTimeNeeded());
 							attraction.exit(p);
 						}
-					}	
+					}
 				}
-					
+
 				else if (type_choice ==1 ){ //choose store
-					ArrayList<LandElement> stores = getAllType(LandObjs.get(land_choice), 's');
-					if (stores.size()==0){
-						emptyLands.add(land_choice);
-						if (emptyLands.size() != LandObjs.size()){
-							do{
-								land_choice = generator.nextInt(num_Lands);	
-							}
-							while(!emptyLands.contains(land_choice));
-						}
-						else
-							p.leavePark();
-						}
+					//*********************************************************
+					//TESTING CODE.  REMOVE ALL CODE BETWEEN HERE AFTER TESTING
+					//*********************************************************
+					System.out.println("Wants to go shopping.");
+					//*********************************************************
+					ArrayList<LandElement> stores = getPossibleChoices(land_choice, 's');
+					if(stores.size()==0){
+						p.leavePark();
+					}
 					else{
 						int choice = generator.nextInt(stores.size());
 						LandElement store = stores.get(i);
 						//Account for time to move to location
 						if(LandObjs.indexOf(store.getLand()) == p.getLocation() + 1)
-								p.decreaseTick(1);
+							p.decreaseTick(1);
 						else
 							p.decreaseTick(2);
-						
+
 						if(store.canEnter(p)){
 							store.enter(p);
+							//*********************************************************
+							//TESTING CODE.  REMOVE ALL CODE BETWEEN HERE AFTER TESTING
+							//*********************************************************
+							System.out.println("Entered Store!");
+							//*********************************************************
 							p.decreaseTick(store.getTimeNeeded());
 							store.exit(p);
 						}
 					}	
-					
+
 				}
 				else { //choose restaurant
-					ArrayList<LandElement> restaurants = getAllType(LandObjs.get(land_choice), 'r');
-					if (restaurants.size()==0){
-						emptyLands.add(land_choice);
-						if (emptyLands.size() != LandObjs.size()){
-							do{
-								land_choice = generator.nextInt(num_Lands);	
-							}
-							while(!emptyLands.contains(land_choice));
-						}
-						else
-							p.leavePark();
-						}
+					//*********************************************************
+					//TESTING CODE.  REMOVE ALL CODE BETWEEN HERE AFTER TESTING
+					//*********************************************************
+					System.out.println("Wants to eat.");
+					//*********************************************************
+					ArrayList<LandElement> restaurants = getPossibleChoices(land_choice, 'r');
+					if(restaurants.size()==0){
+						p.leavePark();
+					}
 					else{
 						int choice = generator.nextInt(restaurants.size());
 						LandElement restaurant = restaurants.get(i);
@@ -190,21 +191,26 @@ public class Park
 							p.decreaseTick(1);
 						else
 							p.decreaseTick(2);
-						
+
 						if(restaurant.canEnter(p)){
 							restaurant.enter(p);
+							//*********************************************************
+							//TESTING CODE.  REMOVE ALL CODE BETWEEN HERE AFTER TESTING
+							//*********************************************************
+							System.out.println("Entered Restaurant!");
+							//*********************************************************
 							p.decreaseTick(restaurant.getTimeNeeded());
 							restaurant.exit(p);
 						}
 					}	
 				}
-				
+
 				if (p.getEnergyLevel() <= 1){
 					p.leavePark();
 				}
 			}
-		//ADD Get Revenue for each store and restaurant	
-			
+			//ADD Get Revenue for each store and restaurant	
+
 		}
 		return result-cost;
 	}
@@ -219,30 +225,55 @@ public class Park
 		}
 		return land_objects;
 	}
-	
+
 	public int getHours()
 	{
 		return hours;					
 	}
-	
+
 	public void setHours(int hours)
 	{
 		this.hours = hours;
 	}
-	
+
 	public double getSalary()
 	{
 		return salary;
 	}
-	
+
 	public void setSalary(double salary)
 	{
 		this.salary = salary;
 	}
-	
+
 	public void add(Land t)
 	{
 		LandObjs.add(t);
 	}
-	
+
+	private ArrayList<LandElement> getPossibleChoices(int land_choice, char type){
+		boolean isChoosing= true;
+		int num_Lands = LandObjs.size();
+		ArrayList<LandElement> choices = new ArrayList<LandElement>();
+		while(isChoosing){
+			choices = getAllType(LandObjs.get(land_choice), type);
+			//An ArrayList of lands that do not contain elements of this choice
+			ArrayList<Integer> emptyLands = new ArrayList<Integer>();
+			Random generator = new Random();
+			if (choices.size()==0){
+				emptyLands.add(land_choice);
+				if (emptyLands.size() != num_Lands){
+					do{
+						land_choice = generator.nextInt(num_Lands);	
+					}
+					while(emptyLands.contains(land_choice));
+				}
+				else
+					return choices;
+			}
+			else
+				isChoosing = false;
+		}
+		return choices;
+	}
 }
