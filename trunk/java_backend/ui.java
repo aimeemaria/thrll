6ceaa7fedbafb4bc2f020664291b43773329cff9
mrprlt99 <mrprlt99@15e78a1c-1ffc-11df-970b-617f64f7e5c1>
2010-05-04 07:05:54 +0000

    gui

--- trunk/java_backend/ui.java
+++ trunk/java_backend/ui.java
@@ -1,11 +1,14 @@
-import javax.swing.*;
-import java.awt.*;
 
 public class ui {
-	public static void main (String[] args){
+	public static void main (String[] args) throws InterruptedException{
 	
 		TextAreaDemo text = new TextAreaDemo();
 		text.setVisible(true); 
+		//Thread.sleep(5000);
+		//System.out.println("onesec");
+		//text.paintppl();
+		//text.repaint();
+		//text.setVisible(true);
 		
 	}
 }
