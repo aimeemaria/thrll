3e13b3f4fe7ddc84ecffa23930838a4f43255c6b
hcmurthy <hcmurthy@15e78a1c-1ffc-11df-970b-617f64f7e5c1>
2010-05-05 19:06:20 +0000

    gui runs everything

---
+++ trunk/gui/thrill_run.sh
@@ -0,0 +1,42 @@
+# !/usr/bin
+
+thrill_source=ThrillProgram.java
+thrill_class=ThrillProgram.class
+
+# Compile the input to generate the intermediate code - ThrillProgram.java
+java -cp ./thrill_parser.jar Parser code.txt
+
+if [ -f $thrill_source ]
+then
+    echo $result
+else
+    echo $result
+
+    #clean up the files now
+    rm -rf ThrillProgram.*
+    exit
+fi
+
+result=`exec javac -cp ./thrill.jar ThrillProgram.java`
+
+if [ -f $thrill_class ]
+then
+    jar uf thrill.jar ThrillProgram.class
+    echo " Welcome to THRLL "
+    echo
+    java -cp ./thrill.jar ThrillProgram
+    echo    
+else
+    echo "Sorry, $thrill_class file does not exist"
+    
+    #clean up the files now
+    rm -rf ThrillProgram.*
+    exit
+fi
+
+#clean up the files now
+rm -rf ThrillProgram.*
+
+
+    
+
