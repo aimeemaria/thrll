229edaf5c78a7dc8c120e6dd61e7b568fa1f125c
hcmurthy <hcmurthy@15e78a1c-1ffc-11df-970b-617f64f7e5c1>
2010-05-08 02:20:06 +0000

    test scripts2

--- trunk/tests/runalltests.sh
+++ trunk/tests/runalltests.sh
@@ -1,20 +1,36 @@
 # !/usr/bin
 
 rundir=../gui
+result=.result
 testList=$1
 passRate=0
 testFiles=`cat $testList`
 count=1
-# build the parser, create jars etc - initial setup
-$rundir/gui_build.sh
+
+# remove the results
+rm -rf results*
+
+if [ $# -eq 2 ]
+then
+    # if build command is issued, then call the build script
+    # which will build the parser, create jars etc - initial setup
+    echo "Building the thrill parser, thrill backend, creating required jars."
+    $rundir/gui_build.sh
+else
+    echo "Build skipped. Running all the tests."
+fi
+
 mkdir results
 
 for testFile in $testFiles
 do
-    echo "Running test $testFile"
-    $rundir/runTest.sh $testFile > resultFile_$count
-    mv resultFile_$count results/
-    count=`expr $count + 1`
+    echo "Running test $testFile."
+    $rundir/runTest.sh $testFile > testFile$result
 done
 
+# move the results to a different folder
+mv *$result results/
+
+# create a tar.gz file
+tar -czf result.tar.gz results/
 
